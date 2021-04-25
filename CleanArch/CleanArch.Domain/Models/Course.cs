efad3906192ef832c460c03d56a4a792663e2e84
Abanoub  Rezk <abanoub_makram_rezk@outlook.com>
2021-04-25 23:13:18 +0200

    commit add images

--- CleanArch/CleanArch.Domain/Models/Course.cs
+++ CleanArch/CleanArch.Domain/Models/Course.cs
@@ -11,6 +11,6 @@ namespace CleanArch.Domain.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public int ImageURL { get; set; }
+        public string ImageURL { get; set; }
     }
 }
---
+++ CleanArch/CleanArch.Infra.Data/Migrations/20210425211140_changeImgarURLDATATYpe.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArch.Infra.Data.Migrations
+{
+    public partial class changeImgarURLDATATYpe : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageURL",
+                table: "Courses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ImageURL",
+                table: "Courses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
