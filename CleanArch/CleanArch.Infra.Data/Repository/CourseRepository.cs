84188ae5a1c2117faaa9d468ee5df823813f6eea
Abanoub  Rezk <abanoub_makram_rezk@outlook.com>
2021-04-25 22:06:31 +0200

    commit icourserepository

---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDBContext universityDBContext;
+
+        public CourseRepository(UniversityDBContext universityDBContext)
+        {
+            this.universityDBContext = universityDBContext;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return universityDBContext.Courses;
+        }
+    }
+}
