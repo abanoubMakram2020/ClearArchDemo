a26efd65b1eb99a46d74223455b17d66f0cd2fe4
Abanoub  Rezk <abanoub_makram_rezk@outlook.com>
2021-04-26 00:09:29 +0200

    addd view

---
+++ CleanArch/CleanArch.MVC/Controllers/CourseController.cs
@@ -0,0 +1,27 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.MVC.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private readonly ICourseService courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            this.courseService = courseService;
+        }
+        public IActionResult Index()
+        {
+            CourseViewModel model = courseService.GetCourses();
+            return View(model);
+        }
+    }
+}
